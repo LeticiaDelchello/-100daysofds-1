20e81c2e9b9275b6902a332ed158627757661437
Leticia <leticia.delchello@gmail.com>
2024-11-04 20:21:08 -0300

    Testando diferentes tipos de saída para diferentes tipos de variáveis

---
+++ testando-saidas.py
@@ -0,0 +1,55 @@
+#Saída em Tipo Inteiro
+idade = 20
+ano = 2010
+
+print(type(idade))
+print(type(ano))
+
+#Saída em Tipo Decimal ou Ponto Flutuante
+altura = 1.73
+peso = 78.500
+
+print(type(peso))
+print(type(altura))
+
+#Saída em Tipo Complexo
+a = 10+16j
+b = 3+80j
+
+print(type(a))
+print(type(b))
+
+#Saída em Tipo String
+nome = 'Guilherme'
+profissao = 'Engenheiro de Software'
+
+print(type(nome))
+print(type(profissao))
+
+#Saída em Tipo Boolean
+sexta_feira = True
+feriado = False
+
+print(type(sexta_feira))
+print(type(feriado))
+
+#Saída em Tipo Listas
+alunos = ['Mônica', 'Ana', 'Bruno', 'Alice']
+notas = [10, 8.5, 7.8, 8.0]
+
+print(type(alunos))
+print(type(notas))
+
+#Saída em Tipo Tupla
+valores = (80, 29, 45, 91, 23)
+pontos = (10, 20.05, 66.8, 72)
+
+print(type(valores))
+print(type(pontos))
+
+#Saída em Tipo Dicionário
+altura = {'Sandra':1.65, 'Elizabeth':1.60, 'Roberto':1.70}
+idade = {'Sandra':35, 'Elizabeth':58, 'Roberto':68}
+
+print(type(altura))
+print(type(idade))
