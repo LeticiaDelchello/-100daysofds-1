389d06423f4665c94ad3e1d129e9a4edef361ef2
Leticia <leticia.delchello@gmail.com>
2024-11-04 20:25:20 -0300

    Convertendo Variáveis em Python

---
+++ converter-variaveis.py
@@ -0,0 +1,22 @@
+#Antes de converter
+altura = 1.55
+print(type(altura))
+
+#Fazenda a conversão
+altura = str(altura)
+
+#Depois da conversão
+print(type(altura))
+print(altura)
+
+
+#Antes de converter
+idade = 18
+print(type(idade))
+
+#Fazendo a conversão
+idade = float(idade)
+
+#Depois da conversão
+print(type(idade))
+print(idade)
